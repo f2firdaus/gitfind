03c0a5e4ec4d4f8fafd0668a9247c17c6f674ffd
f2firdaus <firdausnaseem@gmail.com>
2023-08-01 08:11:50 +0530

    3rd commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     
     return (
         <div className="card footer">
-            <h2>GitFind | Developed by firdaus &amp; Team | &copy; 2022</h2>
+            <h2>GitFind | Developed by firdaus &amp; Team | &copy; 2023</h2>
       <a href="https://github.com/f2firdaus/gitfind" target="_blank" rel="noreferrer">Github Source Code</a>
       <p> Made with ❤️ &amp; 💻</p>
             
